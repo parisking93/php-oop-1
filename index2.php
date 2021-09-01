035871fb15e5ffe1c3640519fcf830cbf0181c73
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-01 16:34:48 +0200

    add metodi setVote e getVote

--- index2.php
+++ index2.php
@@ -4,24 +4,55 @@ vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori 
 
 <?php 
 
+// classe 
 class Movie {
     public $title;
     public $genre;
     public $price;
-    public $link_image;
+    public $linkImage;
     public $vote;
     public $description;
+    public $counter = 0;
+    public $sumVote;
 
+    // costruttore 
     function __construct($_title, $_genre, $_price){
         $this->title = $_title;
         $this->genre = $_genre;
         $this->price = $_price . '$';
     }
 
+    // metodi 
+
+    //do il voto  al film
+    public function setVote($vote) {
+        $this->sumVote += $vote;
+        $this->counter += 1;
+        $this->vote = round($this->sumVote / $this->counter,2);
+    }
+
+    //prendo il voto del film
+    public function getVote() {
+        return $this->vote;
+    }
+    // fine metodi 
+
 }
+// fine classe 
 
 $movie1 = new Movie('Matrix', 'Action', 35);
 $movie2 = new Movie('Pokemon', 'Animation', 12);
+$avgVote = $movie1-> setVote(5);
+$avgVote = $movie1-> setVote(5);
+$avgVote = $movie1-> setVote(2);
+
+$avgVote2 = $movie2-> setVote(2);
+$avgVote2 = $movie2-> getVote();
+
+
+
+echo 'Media voto Movie ' . $avgVote2;
+
 
 var_dump($movie1);
 var_dump($movie2);
